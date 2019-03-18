d54d61dc0703d125fd1e4f2a428887bff6f346c2
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-18 17:22:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+
+  def initialize(school)
+    @school = school
+  end
+
+  def school
+    @school
+  end
+
+  def add_student(student_name, grade)
+  roster = {}
+  roster["grade"] = []
+  roster["grade"] << "student_name"
+  roster.each do |name, grade|
+    puts "#{name}: #{grade}"
+    end
+  end
+end
+
+school = School.new("Bayside High School")
